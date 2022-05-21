d832fff7c5d6bc344729663b4a5c3cd7d672d0be
RaquelDeliG <104938244+RaquelDeliG@users.noreply.github.com>
2022-05-21 17:07:55 +0200

    Add files via upload

---
+++ upgrade-basic-algorithmsit3.js
@@ -0,0 +1,26 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables y = 10 y z = 5.
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5.
+
+let mult = 10 * 5;
+alert(mult);
+let div = 10 / 2;
+alert(div);
+let resto = 15 % 9;
+alert(resto);
+
+let y = 10;
+let z = 5;
+let suma = y + z;
+console.log(suma);
+let multiplicacion = y * z;
+console.log(multiplicacion);
+
